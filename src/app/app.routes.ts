8cd4b88a7d0e9a1d3f403416b195f3f638c11c6a
A07Ponce <ajs.ponce@yavirac.edu.ec>
2024-12-15 23:45:48 -0500

    deberes completos de 1 al 5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { TareasCuadradosComponent } from './tareas-cuadrados/tareas-cuadrados.co
 import { TareaCardsComponent } from './tarea-cards/tarea-cards.component';
 import { TareaFormularioComponent } from './tarea-formulario/tarea-formulario.component';
 import { TareaCarruselComponent } from './tarea-carrusel/tarea-carrusel.component';
+import { Deber1Component } from './deber1/deber1.component';
 
 export const routes: Routes = [
     {path: 'cuadrados', component: TareasCuadradosComponent},
     {path: 'cards', component: TareaCardsComponent},
     {path: 'formulario', component: TareaFormularioComponent},
-    {path: 'carrusel', component: TareaCarruselComponent}
+    {path: 'carrusel', component: TareaCarruselComponent},
+    {path: 'deber1', component: Deber1Component}
 ];
---
+++ src/app/deber1/deber1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deber1',
+  imports: [],
+  templateUrl: './deber1.component.html',
+  styleUrl: './deber1.component.css'
+})
+export class Deber1Component {
+
+}
